d56a0f7cc97a55f4bc18ac18fe1dd3295df0f0b0
Kevin Li <kevinli020508@gmail.com>
2025-10-17 21:48:14 +0000

    Update easydel_server.py to start an OpenAI api server

--- servers/easydel_server.py
+++ servers/easydel_server.py
@@ -1,37 +1,50 @@
+import time
+
 import easydel as ed
-from transformers import AutoTokenizer
-import jax.numpy as jnp
 
-# Initialize model
-model = ed.AutoEasyDeLModelForCausalLM.from_pretrained(
-    "meta-llama/Llama-3.1-8B",
-    dtype=jnp.float16,
-    platform=ed.EasyDeLPlatforms.JAX,
-    auto_shard_model=True,
-    sharding_axis_dims=(1, 1, -1, 1)
+# add in reserve_tokens
+max_model_len = 2048 + 800
+elarge = ed.eLargeModel(
+    {
+        "model": {
+            "name_or_path": "meta-llama/Llama-3.1-8B",
+            "tokenizer": "meta-llama/Llama-3.1-8B",
+        },
+        "sharding": {
+            "axis_dims": (1, 1, 1, -1, 1),
+            "axis_names": ("dp", "fsdp", "ep", "tp", "sp"),
+            "auto_shard_model": True,
+        },
+        "esurge": {
+            "enable_prefix_caching": True,
+            "hbm_utilization": 0.85,
+            "max_model_len": max_model_len,
+            "max_num_seqs": 512,
+            "min_input_pad": 128,
+            "page_size": 128,
+            "verbose": False,
+        },
+        "loader": {"dtype": "bf16", "param_dtype": "bf16", "precision": "DEFAULT"},
+        "base_config": {
+            "values": {
+                "mask_max_position_embeddings": max_model_len,
+                "freq_max_position_embeddings": max_model_len,
+                "attn_mechanism": "auto",
+                "use_pallas_group_matmul": False,
+                "gradient_checkpointing": "checkpoint_dots",
+            }
+        },
+        "eval": {"apply_chat_template": False, "temperature": 0.0, "max_new_tokens": max_model_len},
+    }
 )
+surge = elarge.build_esurge()
+surge.start_monitoring()
+# surge.generate(
+#     ["something here so u should continue generating things like this non stop :)) " for _ in range(3000)],
+#     sampling_params=ed.SamplingParams(max_tokens=1024),
+# )
+# time.sleep(50)
 
-# Setup tokenizer
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-instruct")
-tokenizer.pad_token_id = tokenizer.eos_token_id
-
-# Create inference engine
-sampling_params=ed.SamplingParams(
-    max_tokens=1024,
-    temperature=0.8,
-    top_p=0.95,
-    top_k=10,
-)
-inference = ed.vInference(
-    model=model,
-    processor_class=tokenizer,
-    generation_config=ed.vInferenceConfig(
-        max_new_tokens=1024,
-        sampling_params=sampling_params,
-        streaming_chunks=32,
-    )
-)
-
-# Create API server (OpenAI compatible)
-api_server = ed.vInferenceApiServer({inference.inference_name: inference})
-api_server.fire()
+# Launch API server
+server = ed.eSurgeApiServer(surge)
+server.fire(host="127.0.0.1", port=8000)
